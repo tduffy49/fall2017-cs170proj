253ec4a757a4f0385438600c505441d1deabee9d
Bao Mai <bao.m116@gmail.com>
2017-11-10 16:06:23 -0800

    generate_all_constraints and randomly_select_constraints

--- all_constraints.py
+++ all_constraints.py
@@ -1,12 +1,22 @@
 import random
 
 def generate_all_constraints(lst):
+    """
+    Generate all constraints where (a, b, c) means c not between a and b in `lst`,
+    but does not include duplicates of type (a, a, b) (a, b, a) (a, a, a), (b, a, a).
+
+    :param lst: ordered list for us to generate constraints
+    :return: all constraints with no duplicates of types above
+    """
     constraints = []
     for i in range(len(lst)):
         for j in range(i):
             for k in range(j):
-                constraints.append((lst[k], lst[j], lst[i]))
-                constraints.append((lst[j], lst[k], lst[i]))
+                rand = random.random()  # Add one or the other.
+                if (rand > 0.5):
+                    constraints.append((lst[k], lst[j], lst[i]))
+                else:
+                    constraints.append((lst[j], lst[k], lst[i]))
         for j in range(i + 1, len(lst)):
             for k in range(i + 1, j):
                 constraints.append((lst[k], lst[j], lst[i]))
@@ -14,16 +24,15 @@ def generate_all_constraints(lst):
     return constraints
 
 def randomly_select_constraints(constraints, n):
+    """
+
+    :param constraints: the list of 3-element constraint tuples
+    :param n: number of constraints selected
+    :return: n-sized list of randomly picked elements from `constraints`
+    """
     selected = []
     for i in range(n):
         index = random.randint(0, len(constraints))
         c = constraints.pop(index)
         selected.append(c)
     return selected
-
-constraints = generate_all_constraints(range(1, 51))
-random_constraints = randomly_select_constraints(constraints, 500)
-for c in random_constraints:
-    print(c)
-
-    
