29619ab07a0b5fba167c096de4f26bc0de258382
Bao Mai <bao.m116@gmail.com>
2017-11-19 15:42:45 -0800

    Added interface to main function for testing

---
+++ src/solver.py
@@ -0,0 +1,8 @@
+def solve(n, constraints):
+    """
+
+    :param n: total number of unique wizards
+    :param constraints: list of constraints
+    :return: ordering of `n` wizards that satisfy all constraints
+    """
+    return NotImplementedError
