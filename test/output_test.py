3f05bd8040cb2463f65dcc7c47b5a46d42d8ed7b
Bao Mai <baomai@berkeley.edu>
2017-11-25 18:47:14 -0800

    Write output files.

--- test/output_test.py
+++ test/output_test.py
@@ -5,14 +5,22 @@ import os.path
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 
-from solver import read_input, solve
+from solver import read_input, write_output, solve
 from sat_test import check
 
+# Determines whether tests should also write output of solution to file.
+SHOULD_WRITE_OUTPUT = True
 
 def test_passed_input(files, dir):
     for file in files:
         num_wizards, num_constraints, wizards, constraints = read_input(os.path.join(dir, file))
         solution = solve(num_wizards, num_constraints, wizards, constraints)
+
+        if SHOULD_WRITE_OUTPUT and check(constraints, solution):
+            out_dir = dir.replace('input', 'output')
+            out_file = file.replace('.in', '.out')
+            write_output(os.path.join(out_dir, out_file), solution)
+            print (out_file + ' written.')
 
         return check(constraints, solution)
 
@@ -40,7 +48,6 @@ class OutputTest(unittest.TestCase):
             self.assertTrue(test_passed_input(files, self.DIR_INPUTS_35))
             print ('\n' + file + ' passed.')
 
-    @unittest.skip('performance issues...')
     def test_inputs50(self):
         files = os.listdir(self.DIR_INPUTS_50)
         files = sorted(files, key=str.lower)
