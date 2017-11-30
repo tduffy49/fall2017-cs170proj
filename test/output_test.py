842a51aec039584779cf71d8bae0d8a6de1b1592
Bao Mai <baomai@berkeley.edu>
2017-11-29 17:25:10 -0800

    Refactored output testing

--- test/output_test.py
+++ test/output_test.py
@@ -9,7 +9,8 @@ from solver import read_input, write_output, solve
 from sat_test import check
 
 # Determines whether tests should also write output of solution to file.
-SHOULD_WRITE_OUTPUT = True
+SHOULD_WRITE_OUTPUT = False
+
 
 def test_passed_input(files, dir):
     for file in files:
@@ -23,36 +24,22 @@ def test_passed_input(files, dir):
 
         return check(constraints, solution)
 
+
 class OutputTest(unittest.TestCase):
     DIR_INPUTS_20 = "phase2_inputs/inputs20"
     DIR_INPUTS_35 = "phase2_inputs/inputs35"
     DIR_INPUTS_50 = "phase2_inputs/inputs50"
 
-    def setUp(self):
-        return
+    DIR_TESTS = [DIR_INPUTS_20, DIR_INPUTS_35, DIR_INPUTS_50]
 
-    # Quite redundant to separate same code block into 3 test cases. But this is done
-    # for purposes of testing each input set separately.
-    def test_inputs20(self):
-        files = os.listdir(self.DIR_INPUTS_20)
-        files = sorted(files, key=str.lower)
-        for file in files:
-            self.assertTrue(test_passed_input(files, self.DIR_INPUTS_20))
-            print ('\n' + file + ' passed.')
+    def test_inputs(self):
+        for dir in self.DIR_TESTS:
+            files = os.listdir(dir)
+            files = sorted(files, key=str.lower)
+            for file in files:
+                self.assertTrue(test_passed_input(files, dir))
+                print ('\n' + file + ' passed.')
 
-    def test_inputs35(self):
-        files = os.listdir(self.DIR_INPUTS_35)
-        files = sorted(files, key=str.lower)
-        for file in files:
-            self.assertTrue(test_passed_input(files, self.DIR_INPUTS_35))
-            print ('\n' + file + ' passed.')
-
-    def test_inputs50(self):
-        files = os.listdir(self.DIR_INPUTS_50)
-        files = sorted(files, key=str.lower)
-        for file in files:
-            self.assertTrue(test_passed_input(files, self.DIR_INPUTS_50))
-            print ('\n' + file + ' passed.')
 
 if __name__ == '__main__':
     unittest.main()
